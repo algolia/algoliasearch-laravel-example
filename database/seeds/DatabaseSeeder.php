b86d0c56eaa5988af4fe77bef0a8b3aef14a1d5a
Julien Bourdeau <julien@sigerr.org>
2018-04-04 17:25:45 +0200

    Disable Algolia syncing when seeding DB

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Post::disableSearchSyncing();
+
         $this->call(AuthorSeeder::class);
         $this->call(PostSeeder::class);
     }
