c6b23681f558b982244817e80fc901be3e26964d
Julien Bourdeau <julien@sigerr.org>
2018-04-04 17:55:59 +0200

    Index Comments (without softDeleted)

--- app/Comment.php
+++ app/Comment.php
@@ -4,13 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 
 class Comment extends Model
 {
+    use Searchable;
     use SoftDeletes;
 
     public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function shouldBeSearchable()
+    {
+        return !$this->trashed();
     }
 }
--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -6,5 +6,6 @@ $factory->define(App\Comment::class, function (Faker $faker) {
     return [
         'commenter_name' => $faker->name,
         'comment' => $faker->sentences(rand(1, 5), true),
+        'deleted_at' => $faker->boolean(20) ? $faker->dateTimeBetween('-2 months') : null,
     ];
 });
