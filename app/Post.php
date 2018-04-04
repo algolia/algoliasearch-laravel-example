3f173ccb32709c7b5abeddc2552cfc90f4648cd7
Julien Bourdeau <julien@sigerr.org>
2018-04-04 17:26:52 +0200

    Scout: index softdeleted posts

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     use Searchable;
+    use SoftDeletes;
 
     protected $withCount = ['comments'];
 
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,6 +9,6 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'category' => array_random(['project', 'story', 'achievement', 'reaction']),
         'nb_views' => rand(100, 2500),
         'published_at' => $faker->boolean(85) ? $faker->dateTimeBetween('-2 years') : null,
-        'deleted_at' => $faker->boolean(90) ? $faker->dateTimeBetween('-1 years') : null,
+        'deleted_at' => $faker->boolean(10) ? $faker->dateTimeBetween('-1 years') : null,
     ];
 });
