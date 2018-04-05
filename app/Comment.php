94279de650ccf18f0b11ae062eb1bdd51e875fde
Julien Bourdeau <julien@sigerr.org>
2018-04-05 15:25:09 +0200

    Reindex parent Post when new/updated comment
    	This relies on the Laravel touch feature, but only works for
    	BelongsTo and BelongsToMany relationships.

--- app/Comment.php
+++ app/Comment.php
@@ -11,6 +11,16 @@ class Comment extends Model
     use Searchable;
     use SoftDeletes;
 
+    /**
+     * Every time a new comment is added or an existing
+     * comment is updated, Laravel will update the `updated_at`
+     * attributes of the parent posts.
+     * This will trigger the reindex of this post in Algolia.
+     *
+     * To test it, use `php artisan demo:edit:new-comment {postId}`
+     */
+    protected $touches = ['post'];
+
     public function post()
     {
         return $this->belongsTo(Post::class);
